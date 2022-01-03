5c4d3b2d3fb802d9ab493dbe14bf181b386bfbef
Debankita <Debankitadutta82252@gmail.com>
2022-01-03 14:17:07 +0530

    ready for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const http=require('http');
+const dotenv = require('dotenv').config({path:'./.env'});
 const server=http.createServer(app);
 const socketIo=require('socket.io');
 const formatMessage=require('./utils/messages');
